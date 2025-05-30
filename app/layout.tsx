49a120af1f6152c03a49f81f74b68847e4096d81
asus <sidratulakbar@gmail.com>
2025-05-30 14:04:44 +0800

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
+'use client'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Alenka Properti",
-  description: "Website resmi Alenka Propeti",
-  icons: {
-    icon: "/icon.ico",
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Alenka Properti",
+//   description: "Website resmi Alenka Propeti",
+//   icons: {
+//     icon: "/icon.ico",
+//   },
+// };
 
 export default function RootLayout({
   children,
@@ -19,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
